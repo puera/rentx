d8ae40ebca641749f4d372b1f759a82c2e4e0892
Renann Souza <renann_puera@hotmail.com>
2021-11-28 19:04:40 -0300

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -5,18 +5,10 @@ module.exports = {
   ],
   plugins: [
     ["module-resolver", { alias: {
-      "@modules": [
-        "./src/modules"
-      ],
-      "@config": [
-        "./src/config"
-      ],
-      "@shared": [
-        "./src/shared"
-      ],
-      "@utils": [
-        "./src/utils"
-      ]
+      "@modules": "./src/modules",
+      "@config": "./src/config",
+      "@shared": "./src/shared",
+      "@utils": "./src/utils"
     }
    }
   ],
